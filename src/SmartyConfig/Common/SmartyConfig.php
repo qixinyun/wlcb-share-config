94faeece024cd05efd22b646cdc57005d3e76514
wufeining <wufeining@163.com>
2020-12-28 16:30:14 +0800

    #0-msg-项目初始化

--- src/SmartyConfig/Common/SmartyConfig.php
+++ src/SmartyConfig/Common/SmartyConfig.php
@@ -6,9 +6,9 @@ class SmartyConfig
     public static function configDir()
     {
         $dirArray = array(
-            S_ROOT.'vendor/qixinyun/lvliang-share-config/src/SmartyConfig/WLCB',
-            S_ROOT.'vendor/qixinyun/lvliang-share-config/src/SmartyConfig/Common',
-            S_ROOT.'vendor/qixinyun/lvliang-share-config/src/SmartyConfig/',
+            S_ROOT.'vendor/qixinyun/wlcb-share-config/src/SmartyConfig/WLCB',
+            S_ROOT.'vendor/qixinyun/wlcb-share-config/src/SmartyConfig/Common',
+            S_ROOT.'vendor/qixinyun/wlcb-share-config/src/SmartyConfig/',
         );
 
         return $dirArray;
